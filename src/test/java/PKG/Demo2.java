61bd1588ba223f540f1d61d5e1c013eb12eed3be
jackvelazquez <jose.velazquez@gmail.com>
2021-06-27 21:11:12 -0500

    GIT 9:11 pm

--- src/test/java/PKG/Demo2.java
+++ src/test/java/PKG/Demo2.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class Demo2 {
 
     @Test(priority=2, groups = "sanity")
-    public void prueba1()
+    public void prueba99()
     {
         System.out.println("Prueba 1");
         Assert.assertTrue(true);
