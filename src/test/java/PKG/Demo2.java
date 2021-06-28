a65ad133e721e565b2b0d663f627bd9dff9fd3cb
jackvelazquez <jose.velazquez@gmail.com>
2021-06-27 21:08:12 -0500

    GIT 9:08 pm

--- src/test/java/PKG/Demo2.java
+++ src/test/java/PKG/Demo2.java
@@ -3,7 +3,7 @@ package PKG;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SeleniumTest {
+public class Demo2 {
 
     @Test(priority=2, groups = "sanity")
     public void prueba1()
