6f9a7727517272ba1b336cb356aeb4b70967a927
jackvelazquez <jose.velazquez@gmail.com>
2021-06-27 19:16:46 -0500

    Initial commit

---
+++ src/test/java/PKG/ClaseDemo.java
@@ -0,0 +1,53 @@
+package PKG;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class ClaseDemo {
+
+    @Test (priority=2)
+    public void prueba1()
+    {
+        System.out.println("Prueba 1");
+        Assert.assertTrue(true);
+    }
+
+    @Test (priority=1)
+    public void prueba2()
+    {
+        System.out.println("Prueba 2");
+        Assert.assertTrue(true);
+    }
+
+    @Test (priority=1)
+    public void prueba3()
+    {
+        System.out.println("Prueba 3");
+        Assert.assertEquals("Resultado1","Resultado1");
+
+        System.out.println("Prueba 3, despues del soft assertion...");
+        // Assert.assertTrue(true);
+    }
+
+    @Test (priority=4)
+    public void prueba4()
+    {
+        SoftAssert saPrueba = new SoftAssert();
+
+        System.out.println("Prueba 4, antes del soft assertion 1...");
+        saPrueba.assertEquals("Resultado1","Resultado1");
+        Reporter.log("Error en el log #1 ", true);
+        System.out.println("Prueba 4, despues del soft assertion 1...");
+
+        System.out.println("Prueba 4, antes del soft assertion 2...");
+        saPrueba.assertEquals("Resultado1","Resultado1");
+        Reporter.log("Error en el log #2 ", true);
+        System.out.println("Prueba 4, despues del soft assertion 2 ..");
+
+        System.out.println("Asertions: ");
+        saPrueba.assertAll();
+    }
+
+}
